dbc8522d9618a61c6243be5032584bad9fff71ab
Thor100779 <grot0130@umn.edu>
2021-09-08 22:36:54 -0500

    Add scrolling behavior to nav bar links.

--- js/app.js
+++ js/app.js
@@ -5,22 +5,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
       // Dynamically add sections to header bar
       let heading = document.createElement('li');
       heading.setAttribute('id', `header_section_${i}`);
-      heading.innerHTML = `<b>Section${i}</b>`;
+      heading.innerHTML = `<a href="#feature_${i}"><b>Section${i}</b></a>`;
       headerSections.appendChild(heading);
 
       // Set sections up to have collpase/expand behavior
       let collapseButton = document.getElementById(`feature_${i}_min_max`);
       collapseButton.addEventListener('click', event => {
         let featureSection = document.getElementById(`feature_${i}_content`);
-        console.log(`display is currently ${featureSection.getAttribute('display')}`);
 
         if (featureSection.classList.contains("active")) {
           featureSection.style.display = 'none';
           featureSection.classList.remove("active");
+          collapseButton.setAttribute("src", "images/expand_icon.jpg");
         }
         else {
           featureSection.style.display = 'block';
           featureSection.classList.add("active");
+          collapseButton.setAttribute("src", "images/collapse_icon.jpg");
         }
       });
     }
